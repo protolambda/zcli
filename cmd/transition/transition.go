5975c647a6183b886d72213397023c7ee05fb9ca
protolambda <proto@protolambda.com>
2019-09-04 10:02:33 -0400

    cleanup, write errs to stderr

--- cmd/transition/transition.go
+++ cmd/transition/transition.go
@@ -23,7 +23,7 @@ func init() {
 	TransitionCmd.PersistentFlags().String("post", "", "Post (Output) path. If none is specified, output is written to STDOUT")
 
 	SlotsCmd = &cobra.Command{
-		Use: "slots <number>",
+		Use:   "slots <number>",
 		Short: "Process empty slots on the pre-state to get a post-state",
 		Args: func(cmd *cobra.Command, args []string) error {
 			if len(args) != 1 {
@@ -37,49 +37,68 @@ func init() {
 		},
 		Run: func(cmd *cobra.Command, args []string) {
 			isDelta, err := cmd.Flags().GetBool("delta")
-			check(err, "delta flag could not be parsed")
+			if check(err, cmd.ErrOrStderr(), "delta flag could not be parsed") {
+				return
+			}
 
 			slots, _ := strconv.ParseUint(args[0], 10, 64)
 
-			state := loadPreFull(cmd)
+			state, err := loadPreFull(cmd)
+			if check(err, cmd.ErrOrStderr(), "pre state could not be loaded") {
+				return
+			}
 
 			to := core.Slot(slots)
 			if isDelta {
 				to += state.Slot
 			} else if to < state.Slot {
-				_, _ = fmt.Fprintln(os.Stderr, "to slot is lower than pre-state slot")
+				report(cmd.ErrOrStderr(), "to slot is lower than pre-state slot")
 				return
 			}
 
 			state.ProcessSlots(to)
-
-			writePost(cmd, state.BeaconState)
+			err = writePost(cmd, state.BeaconState)
+			if check(err, cmd.ErrOrStderr(), "could not write post-state") {
+				return
+			}
 		},
 	}
 	SlotsCmd.Flags().Bool("delta", false, "to interpret the slot number as a delta from the pre-state")
 	TransitionCmd.AddCommand(SlotsCmd)
 
-
 	BlocksCmd = &cobra.Command{
 		Use:   "blocks",
 		Short: "Process blocks on the pre-state to get a post-state",
-		Args: cobra.ArbitraryArgs,
+		Args:  cobra.ArbitraryArgs,
 		Run: func(cmd *cobra.Command, args []string) {
 			verifyStateRoot, err := cmd.Flags().GetBool("verify-state-root")
-			check(err, "verify-state-root could not be parsed")
+			if check(err, cmd.ErrOrStderr(), "verify-state-root could not be parsed") {
+				return
+			}
 
-			state := loadPreFull(cmd)
+			state, err := loadPreFull(cmd)
+			if check(err, cmd.ErrOrStderr(), "could not load pre-state") {
+				return
+			}
 
 			for i := 0; i < len(args); i++ {
-				b := loadBlock(args[i])
+				b, err := loadBlock(args[i])
+				if check(err, cmd.ErrOrStderr(), "could not load block: %s", args[i]) {
+					return
+				}
 
 				blockProc := &phase0.BlockProcessFeature{Block: b, Meta: state}
 
-				err := state.StateTransition(blockProc, verifyStateRoot)
-				check(err, "failed block transition to block %s", args[i])
+				err = state.StateTransition(blockProc, verifyStateRoot)
+				if check(err, cmd.ErrOrStderr(), "failed block transition to block %s", args[i]) {
+					return
+				}
 			}
 
-			writePost(cmd, state.BeaconState)
+			err = writePost(cmd, state.BeaconState)
+			if check(err, cmd.ErrOrStderr(), "could not write post-state") {
+				return
+			}
 		},
 	}
 
@@ -88,65 +107,91 @@ func init() {
 	TransitionCmd.AddCommand(BlocksCmd)
 }
 
-func check(err error, msg string, args ...interface{}) {
+func report(out io.Writer, msg string, args ...interface{}) {
+	_, _ = fmt.Fprintf(out, msg, args...)
+}
+
+func check(err error, out io.Writer, msg string, args ...interface{}) bool {
 	if err != nil {
-		fmt.Printf(msg, args...)
-		fmt.Printf("%v", err)
-		os.Exit(1)
+		report(out, msg, args...)
+		report(out, "%v", err)
+		return true
+	} else {
+		return false
 	}
 }
 
-func loadBlock(blockPath string) *phase0.BeaconBlock {
+func loadBlock(blockPath string) (*phase0.BeaconBlock, error) {
 	r, err := os.Open(blockPath)
-	check(err, "cannot read block from input path: %s", blockPath)
+	if err != nil {
+		return nil, fmt.Errorf("cannot read block from input path: %s\n%v", blockPath, err)
+	}
 
 	var buf bytes.Buffer
 	_, err = buf.ReadFrom(r)
-	check(err, "cannot read block into buffer: %s", blockPath)
+	if err != nil {
+		return nil, fmt.Errorf("cannot read block into buffer: %s\n%v", blockPath, err)
+	}
 
 	var block phase0.BeaconBlock
 	err = zssz.Decode(&buf, uint64(buf.Len()), &block, phase0.BeaconBlockSSZ)
-	check(err, "cannot decode block SSZ: %s", blockPath)
+	if err != nil {
+		return nil, fmt.Errorf("cannot decode block SSZ: %s\n%v", blockPath, err)
+	}
 
-	return &block
+	return &block, nil
 }
 
-func loadPreFull(cmd *cobra.Command) *phase0.FullFeaturedState {
+func loadPreFull(cmd *cobra.Command) (*phase0.FullFeaturedState, error) {
 	inPath, err := cmd.Flags().GetString("pre")
-	check(err, "pre path is invalid")
+	if err != nil {
+		return nil, fmt.Errorf("pre path could not be parsed")
+	}
 
 	var r io.Reader
 	if inPath == "" {
 		r = os.Stdin
 	} else {
 		r, err = os.Open(inPath)
-		check(err, "cannot read pre from input path")
+		if err != nil {
+			return nil, fmt.Errorf("cannot read pre from input path: %v", err)
+		}
 	}
 
 	var buf bytes.Buffer
 	_, err = buf.ReadFrom(r)
-	check(err, "cannot read pre-state into buffer")
+	if err != nil {
+		return nil, fmt.Errorf("cannot read pre-state into buffer: %v", err)
+	}
 
 	var pre phase0.BeaconState
 	err = zssz.Decode(&buf, uint64(buf.Len()), &pre, phase0.BeaconStateSSZ)
-	check(err, "cannot decode pre-state")
+	if err != nil {
+		return nil, fmt.Errorf("cannot decode pre-state: %v", err)
+	}
 
 	preFull := phase0.NewFullFeaturedState(&pre)
 	preFull.LoadPrecomputedData()
-	return preFull
+
+	return preFull, nil
 }
 
-func writePost(cmd *cobra.Command, state *phase0.BeaconState) {
+func writePost(cmd *cobra.Command, state *phase0.BeaconState) error {
 	outPath, err := cmd.Flags().GetString("post")
-	check(err, "post path is invalid")
+	if err != nil {
+		return fmt.Errorf("post path could not be parsed: %v", err)
+	}
 
 	var w io.Writer
 	if outPath == "" {
 		w = os.Stdout
 	} else {
-		w, err = os.OpenFile(outPath, os.O_CREATE | os.O_WRONLY, os.ModePerm)
+		w, err = os.OpenFile(outPath, os.O_CREATE|os.O_WRONLY, os.ModePerm)
 	}
 
 	_, err = zssz.Encode(w, state, phase0.BeaconStateSSZ)
-	check(err, "cannot encode post-state")
+	if err != nil {
+		return fmt.Errorf("cannot encode post-state: %v", err)
+	}
+	return nil
 }
