9dd30ba56fe9cda117f80a6ab86db7b45e5ef973
protolambda <proto@protolambda.com>
2019-09-10 00:22:07 -0400

    crosslinking and attester status bugfixes and version/help printing

--- cmd/root.go
+++ cmd/root.go
@@ -9,11 +9,14 @@ import (
 	"github.com/protolambda/zcli/cmd/pretty"
 	"github.com/protolambda/zcli/cmd/roots"
 	"github.com/protolambda/zcli/cmd/transition"
+	"github.com/protolambda/zcli/util"
+	"github.com/protolambda/zrnt/eth2"
+	"github.com/protolambda/zssz"
 	"github.com/spf13/cobra"
 	"os"
 )
 
-var RootCmd *cobra.Command
+var RootCmd, VersionCmd *cobra.Command
 
 func init() {
 	RootCmd = &cobra.Command{
@@ -21,10 +24,22 @@ func init() {
 		Short: "ZRNT CLI is a tool for ETH 2 debugging",
 		Long:  `A command line tool for ETH 2 debugging, based on ZRNT, the Go exec-spec built by @protolambda.`,
 		Run: func(cmd *cobra.Command, args []string) {
-			// Do Stuff Here
+			_ = cmd.Help()
+		},
+	}
+	VersionCmd = &cobra.Command{
+		Use:   "version",
+		Short: "Print versions of integrated tools",
+		Run: func(cmd *cobra.Command, args []string) {
+			util.Report(cmd.OutOrStdout(), `
+ZCLI: v0.0.8
+ZRNT: `+eth2.VERSION+`
+ZSSZ: `+zssz.VERSION+`
+`)
 		},
 	}
 
+	RootCmd.AddCommand(VersionCmd)
 	RootCmd.AddCommand(genesis.GenesisCmd)
 	RootCmd.AddCommand(transition.TransitionCmd)
 	RootCmd.AddCommand(pretty.PrettyCmd)
--- go.mod
+++ go.mod
@@ -5,8 +5,8 @@ go 1.12
 require (
 	github.com/phoreproject/bls v0.0.0-20190821133044-da95d4798b09
 	github.com/protolambda/messagediff v1.3.0
-	github.com/protolambda/zrnt v0.8.4-2
-	github.com/protolambda/zssz v0.1.2
+	github.com/protolambda/zrnt v0.8.4-3
+	github.com/protolambda/zssz v0.1.3
 	github.com/spf13/cobra v0.0.5
 	gopkg.in/yaml.v2 v2.2.2
 )
