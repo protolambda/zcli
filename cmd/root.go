8c911d95802229b3e4b09651792953965fdfc755
protolambda <proto@protolambda.com>
2019-12-21 14:50:41 +0100

    v0.0.22

--- cmd/root.go
+++ cmd/root.go
@@ -37,7 +37,7 @@ func init() {
 		Short: "Print versions of integrated tools",
 		Run: func(cmd *cobra.Command, args []string) {
 			util.Report(cmd.OutOrStdout(), `
-ZCLI: v0.0.21
+ZCLI: v0.0.22
 ZRNT: `+eth2.VERSION+`
 ZSSZ: `+zssz.VERSION+`
 
