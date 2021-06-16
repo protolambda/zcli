ee87e18b9c1a01070143802bb5b23ced1653db44
protolambda <proto@protolambda.com>
2021-06-16 03:27:39 +0200

    update version command

--- commands/version.go
+++ commands/version.go
@@ -3,13 +3,22 @@ package commands
 import (
 	"context"
 	"fmt"
+	"github.com/protolambda/zrnt/eth2"
 )
 
 type VersionCmd struct {
 }
 
-func (c *VersionCmd) Run(ctx context.Context) error {
-	fmt.Println("Version") // TODO
-	return nil
+const Version = "ZCLI v0.2.0"
 
+func (c *VersionCmd) Help() string {
+	return "Print ZCLI and ZRNT version"
+}
+
+func (c *VersionCmd) Run(ctx context.Context) error {
+	fmt.Printf(`
+ZCLI version: %s
+ZRNT version: %s
+`, Version, eth2.VERSION)
+	return nil
 }
